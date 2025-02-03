8991054a94ec7f96b12cfb67dc9755fa2c91b37f
kingstech001 <mamahkingsleychukwuebuka@gmail.com>
2025-02-03 15:09:34 -0800

    updated nec.config

--- oneecosystem-exchange/next.config.ts
+++ oneecosystem-exchange/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/old-page',
+        destination: '/new-page',
+        permanent: true,
+      },
+    ]
+  },
+}
